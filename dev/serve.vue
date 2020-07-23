f67b4d8468edfe7dc68568601ad32609bf488c4e
vagrant <jamesrichardgriffiths@gmail.com>
2020-07-23 22:07:00 +0000

    changing to rollup

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { LemonComponentsSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    LemonComponentsSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <lemon-components-sample />
+  </div>
+</template>
